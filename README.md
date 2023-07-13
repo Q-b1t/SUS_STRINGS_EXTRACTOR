f7b56874a98d70239a4a8ace5da680966c77018e
Q-b1t <ldhq_rhiq@protonmail.com>
2023-07-12 19:25:56 -0600

    qw

---
+++ README.md
@@ -0,0 +1,14 @@
+# SUS STRING EXTRACTOR
+
+Prototype for a command line interface for further analysing text files containing strings extracted from malware samples. The idea is to symplify malware analysis. The strings that are searched for are patterns belonging to the following categories:
+- Wanacry Language Indicators
+- Windows Directories
+- URLs
+- Binaries & Executables
+- XML tags
+- Possible base 64 strings (there is an option to decode the strings if found)
+- IP Addresses
+- Calls to golang APIs 
+- Known malitious addresses
+
+## Usage
--- string_extractor.py
+++ string_extractor.py
@@ -1,4 +1,7 @@
 import re
+from base64 import b64decode
+from termcolor import colored
+import time 
 
 def extract_usefull_strings(file_name):    
     """
@@ -7,6 +10,7 @@ def extract_usefull_strings(file_name):
     found un malware samples. The patterns are classified in lists, and the function returns a dictionary
     mapping pattern to the list of matching strings found.
     """
+    print(colored(f"[*] Searching for common malware patterns in {file_name}.","blue"))
     # pattern particular for wannacry
     language_pattern = r"^msg/m_([a-z]+).wnry"
     # urls
@@ -23,6 +27,8 @@ def extract_usefull_strings(file_name):
     ip_address_pattern = r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     # possible base 64 pattern
     base64_pattern = r"[a-zA-Z0-9+/,=]{40,}"
+    # golang indicators
+    golang_pattern = r"^golang.org([A-Za-z0-9/.()*]+)"
 
     # lists to store findings
     language_list = list()
@@ -33,6 +39,7 @@ def extract_usefull_strings(file_name):
     user_agent_list = list()
     ip_address_list = list()
     base64_list = list()
+    golang_list = list()
 
     # open file handler
     f = open("strings.txt","r")
@@ -55,6 +62,8 @@ def extract_usefull_strings(file_name):
             ip_address_list.append(line)
         elif re.match(base64_pattern,line):
             base64_list.append(line)
+        elif re.match(golang_pattern,line):
+            golang_list.append(line)
         else:
             continue
 
@@ -66,21 +75,53 @@ def extract_usefull_strings(file_name):
         "xml":xml_list,
         "user_agents":user_agent_list,
         "ip_addresses":ip_address_list,
-        "base64":base64_list
+        "base64":base64_list,
+        "golang_indicators":golang_list
     }
+def show_findings(patterns):
+    for pattern,ocurrences in patterns.items():
+        if len(ocurrences) > 0:
+            print(colored(f"[+] Found {len(ocurrences)} possible ocurrences of {pattern}","green"))
+            time.sleep(3)
 
-def write_output_file(patterns,output_file):
-    pass
+def write_output_file(patterns,output_file,decode,verbose):
+    if decode and len(patterns["base64"]) > 0:
+        print(colored("[*] Will attempt to decode base64 possible ocurrences.","blue"))
+        time.sleep(3)
+    with open(output_file,"w") as f:
+        for pattern,ocurrences in patterns.items():
+            if len(ocurrences) > 0:
+                f.write("*"*50 + " " + pattern.upper().replace("_"," ") + " " + "*"*50 + "\n")
+                for ocurrence in ocurrences:
+                    if ocurrence[-1] != "\n":
+                        ocurrence += "\n"
+                    if pattern == "base64" and decode:
+                        try:
+                            ocurrence = b64decode(ocurrence).decode()
+                            if verbose:
+                                print(colored("[-] Base64 decoding successfull.","green"))
+                        except:
+                            if verbose:
+                                print(colored("[-] Base64 decoding failed.","red"))
+                    f.write(ocurrence)
+            else:
+                continue
+    f.close() 
+    print(colored(f"[+] The findings have beed written to {output_file}.","green"))
 
 
 if __name__ == '__main__':
     input_file = "strings.txt"
     output_file = "classified_strings.txt"
+    decode = True
+    verbose = True
 
     # extract the classified patterns 
     patterns_classified = extract_usefull_strings(input_file)
-
+    # show the findings
+    if verbose:
+        show_findings(patterns_classified)
     # write the patterns to an output file
-    write_output_file(patterns_classified,output_file)
+    write_output_file(patterns_classified,output_file,decode = decode,verbose = verbose)
 
 
